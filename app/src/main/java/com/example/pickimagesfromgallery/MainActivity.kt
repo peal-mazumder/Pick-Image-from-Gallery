7f0ea1614f484017d3768274c396c53a0d76661d
Peal Mazumder <peal6614@gmail.com>
2022-11-08 16:50:19 +0600

    Implement using library

--- app/src/main/java/com/example/pickimagesfromgallery/MainActivity.kt
+++ app/src/main/java/com/example/pickimagesfromgallery/MainActivity.kt
@@ -2,21 +2,28 @@ package com.example.pickimagesfromgallery
 
 import android.Manifest
 import android.app.Activity
+import android.app.AlertDialog
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.net.Uri
 import android.os.Bundle
+import android.provider.MediaStore
+import android.util.Log
 import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import com.example.pickimagesfromgallery.databinding.ActivityMainBinding
+import com.github.drjacky.imagepicker.ImagePicker
+import com.github.drjacky.imagepicker.constant.ImageProvider
+import java.io.File
+
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
-    val IMAGE_PICK_CODE = 2
-    val REQUEST_CODE = 1
+    private val REQUEST_CODE = 1
 
     private val viewModel: MainViewModel by viewModels()
 
@@ -29,49 +36,53 @@ class MainActivity : AppCompatActivity() {
         askForPermission()
         binding.recyclerViewGalleryImage.adapter = adapter
 
-        binding.btnAddPhotos.setOnClickListener {
-            val intent = Intent()
-            intent.type = "image/*"
-            intent.putExtra(Intent.EXTRA_ALLOW_MULTIPLE, true)
-            intent.action = Intent.ACTION_PICK
-            startActivityForResult(intent, IMAGE_PICK_CODE)
-        }
+        val launcher =
+            registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+                Log.d("Peal", "Error")
+                if (it.resultCode == Activity.RESULT_OK) {
+                    Log.d("Peal", "${it.data?.clipData?.itemCount}")
+                    if (it.data?.clipData != null) {
 
-    }
+                        val count = it.data?.clipData?.itemCount
+                        Log.d("Peal", "$count")
+                        for (i in 0 until count!!) {
+                            val imageUri: Uri = it.data?.clipData?.getItemAt(i)!!.uri
+                            viewModel.images.add(imageUri)
+                        }
 
 
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-
-        if (resultCode == Activity.RESULT_OK && requestCode == IMAGE_PICK_CODE) {
-            if (data?.clipData != null) {
-                val count = data.clipData?.itemCount
-
-                for (i in 0 until count!!) {
-                    val imageUri: Uri = data.clipData?.getItemAt(i)!!.uri
-                    viewModel.images.add(imageUri)
+                    } else if (it.data?.data != null) {
+                        val imageUri: Uri = it.data?.data!!
+                        viewModel.images.add(imageUri)
+                    }
+                    adapter.submitList(viewModel.images)
+                    adapter.notifyDataSetChanged()
                 }
-
-
-            } else if (data?.data != null) {
-                val imageUri: Uri = data.data!!
-                viewModel.images.add(imageUri)
             }
-            adapter.submitList(viewModel.images)
-            adapter.notifyDataSetChanged()
+
+        binding.btnAddPhotos.setOnClickListener {
+            ImagePicker.with(this)
+                .setMultipleAllowed(true)
+                .provider(ImageProvider.BOTH)
+                .createIntentFromDialog { launcher.launch(it) }
         }
+
     }
 
     private fun askForPermission() {
         if (ContextCompat.checkSelfPermission(
                 this,
                 Manifest.permission.READ_EXTERNAL_STORAGE
+            ) != PackageManager.PERMISSION_GRANTED || ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.CAMERA
             ) != PackageManager.PERMISSION_GRANTED
         ) {
             ActivityCompat.requestPermissions(
                 this,
                 arrayOf(
-                    Manifest.permission.READ_EXTERNAL_STORAGE
+                    Manifest.permission.READ_EXTERNAL_STORAGE,
+                    Manifest.permission.CAMERA
                 ),
                 REQUEST_CODE
             )
